b17de5e8f5cb81e51dcec378a21a655e87e11bc0
Inooid <justdrawcats@gmail.com>
2014-11-28 12:11:30 +0100

    Set route for change_active_state method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :playlists, except: [:show]
+  post 'playlists/:id' => 'playlists#change_active_state'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
